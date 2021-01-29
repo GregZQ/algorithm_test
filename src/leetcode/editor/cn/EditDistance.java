77407813d6f649a57554e298274ab6b3acdc986d
zjone2 <463803856@qq.com>
2021-01-29 13:59:06 +0800

    ref #master @zjone2 算法练习

--- src/leetcode/editor/cn/EditDistance.java
+++ src/leetcode/editor/cn/EditDistance.java
@@ -53,7 +53,29 @@ public class EditDistance {
     //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minDistance(String word1, String word2) {
-return 1;
+
+        int length1 = word1.length();
+        int length2 = word2.length();
+
+        int dp[][] = new int[length1+1][length2+1];
+        for (int i = 1; i <=length1; i++){
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <=length2; i++) {
+            dp[0][i]=i;
+        }
+
+        for (int i = 1 ; i <= length1; i ++) {
+            for (int j = 1; j <= length2;j ++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i-1][j-1]+1, dp[i-1][j]+1), dp[i][j-1]+1);
+                }
+            }
+        }
+
+        return dp[length1][length2];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
